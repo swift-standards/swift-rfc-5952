4014cf85912e5162e722e13b100de4b39697c303
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 01:15:55 +0100

    Apply swift-format and lint fixes

--- Package.swift
+++ Package.swift
@@ -17,10 +17,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-rfc-5952",
     platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11)
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26)
     ],
     products: [
         .library(name: .rfc5952, targets: [.rfc5952]),
